e0781fd1671267ae206c2209ab9c32e77038afcc
han <icyfarhan@gmail.com>
2025-02-22 14:16:57 +0700

    Completed the Art Space app assignment

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.artspace
 
-import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,32 +8,31 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArtSpaceTheme {
                 Surface() {
-
+                    ArtSpaceApp()
                 }
             }
         }
@@ -57,35 +55,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp() {
+    var currentArtIdentifier by remember { mutableStateOf(1) }
+
+    val currentArt = when (currentArtIdentifier) {
+        1 -> R.drawable.chad
+        2 -> R.drawable.npc
+        3 -> R.drawable.daikichi
+        4 -> R.drawable.wife
+        else -> R.drawable.chad
+    }
+
+    val currentArtDescription = when (currentArtIdentifier) {
+        1 -> R.string.description_chad
+        2 -> R.string.description_npc
+        3 -> R.string.description_daikichi
+        4 -> R.string.description_wife
+        else -> R.string.description_chad
+    }
+
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(24.dp),
+        .padding(24.dp)
+        .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
-        ImageContainer(picture = R.drawable.chad, contentDescription = "The REAL CHADD")
+        ImageContainer(picture = currentArt, contentDescription = currentArtDescription)
 
         Spacer(modifier = Modifier.height(50.dp))
 
-        TextContainer(text = R.string.description_chad)
+        TextContainer(text = currentArtDescription)
 
         Spacer(modifier = Modifier.height(50.dp))
 
         Row(modifier = Modifier
             .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-            MyButton(onClick = { }, text = R.string.previous_btn)
-            MyButton(onClick = { }, text = R.string.next_btn)
+            MyButton(onClick = { currentArtIdentifier = maxOf(1, currentArtIdentifier - 1) }, text = R.string.previous_btn)
+            MyButton(onClick = { currentArtIdentifier = minOf(4, currentArtIdentifier + 1) }, text = R.string.next_btn)
         }
     }
 }
 
 @Composable
-fun ImageContainer(@DrawableRes picture: Int, contentDescription: String) {
+fun ImageContainer(@DrawableRes picture: Int, contentDescription: Int) {
     Box(modifier = Modifier
         .shadow(3.dp, shape = RoundedCornerShape(8.dp))
     ) {
         Image(
             painter = painterResource(picture),
-            contentDescription = contentDescription,
+            contentDescription = stringResource(contentDescription),
             modifier = Modifier
                 .padding(24.dp)
                 .height(400.dp)
